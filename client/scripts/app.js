f6974513f6145b4edcc9728dd6d894164d246138
hirichardhai <hirichardhai@gmail.com>
2018-01-26 19:57:14 -0800

    Load init, added form for text, displayed username and text

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,12 @@
 // YOUR CODE HERE:
 var app = {
-  init: function() {},
+  init: function() {
+    app.fetch();
+  },
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      url: app.server,
       type: 'POST',
       data: message,
       contentType: 'application/json',
@@ -19,41 +21,45 @@ var app = {
   },
   fetch: function() {
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
+      url: app.server,
       type: 'GET',
-      data: message,
-      contentType: 'application/json',
+      data: '',
+      contentType: '',
       success: function (data) {
         console.log('chatterbox: Message sent');
+        console.log(data);
+        app.renderMessage(data.results);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
-    // $.get();
+    // $.get('http://parse.sfm8.hackreactor.com/chatterbox/classes/messages', alert('success'));
   },
+  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
   clearMessages: function() {
     $('#chats').empty();
   },
   renderMessage: function(message) {
     
-    var username = message.username;
-    var text = message.text;
-    var roomname = message.roomname;
+    for (var i = 0; i < message.length; i++) {
+      var username = message[i].username;
+      var text = message[i].text;
+      var roomname = message[i].roomname;
 
-    $('#chats').append('<div>' + '<button href="#" class=' + JSON.stringify(username) + '>' + username + '</button>' + ': ' + text + '</div>');
-    // if ($('#chats').find()
-    $('".' + JSON.stringify(username) + '"').click(function() {
-      app.handleUsernameClick(username);
-    });
+      $('#chats').append('<div>' + '<button href="#" class=username>' + username + '</button>' + ': ' + text + '</div>');
+      $('.username').click(function() {
+        app.handleUsernameClick(username);
+      });
+    }
   },
   renderRoom: function(roomName) {
     $('#roomSelect').append('<div>' + roomName + '</div>');
+    
   },
   handleUsernameClick: function(username) {
     // document.getElementById("username").addEventListener("click", console.log('hi'));
     $('#main').append('<div>' + username + '</div>');
   }
 };
-
