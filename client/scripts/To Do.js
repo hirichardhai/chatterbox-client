7b2601474454895a2eea4c970a7d24b48e189160
hirichardhai <hirichardhai@gmail.com>
2018-01-27 17:12:26 -0800

    Added comments on our app.js and HTML

---
+++ client/scripts/To Do.js
@@ -0,0 +1,14 @@
+To Do List
+
+// clear button
+friendslist
+  add css to friends' chats
+room
+  add behavior upon selection
+
+//escape
+// send message
+  // event listener on submit button
+  // jquery to grab text area text
+  // send that into the app.send()
+html add scroll box
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,15 @@
 // YOUR CODE HERE:
 var app = {
+  //init is for running any code when we load our html
+    //we load init with <body onload='app.init()'>
   init: function() {
     app.fetch();
     setTimeout(function() {
       app.fetch();
     }, 333);
   },
+  // using jQuery, we execute ajax to send objects as strings to 
+    // the app.server.  this is usually called a POST
   send: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
@@ -22,6 +26,10 @@ var app = {
       }
     });
   },
+  // app.fetch is a method that retrieves a JSSON string from the app.server.
+  // in order to see the order of the data, we can use createdAt or -createdAt for chronological order differences
+  // success runs any fucntions with the data JSON we "GET". 
+  // took the data's roomnames and appended the rooms to our #rooms HTML
   fetch: function() {
     $.ajax({
       url: app.server,
@@ -30,11 +38,18 @@ var app = {
       contentType: '',
       success: function (data) {
         console.log('chatterbox: Data loaded');
-        console.log(data);
+        var allRooms = app.sortRooms(data.results);
+        var roomsInArray = Object.keys(allRooms);
+
+
         app.clearMessages();
         app.renderMessage(data.results);
-        var allRooms = app.sortRooms(data.results);
         console.log('allRooms', allRooms);
+        $('#rooms').empty();
+        for (var key in allRooms) {
+          $('#rooms').append('<option value=' + key + '">' + key + "</option>")
+        }
+        
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,6 +62,8 @@ var app = {
   clearMessages: function() {
     $('#chats').empty();
   },
+  // was a callback function used in fetch to display all messages using jQuery
+  // to append to our #chats as well as usernames to .username
   renderMessage: function(message) {
     
     for (var i = 0; i < message.length; i++) {
@@ -60,26 +77,32 @@ var app = {
       });
     }
   },
+  // not functional
   renderRoom: function(roomName) {
     $('#roomSelect').append('<div>' + roomName + '</div>');
     
   },
+  // our attempt to create a database for our rooms using a object. the key is the roomname and the value are all messages with the same room name.
+  // have not implemented change. object also not properly storing objects with no roomname
   sortRooms: function(array) {
     var roomList = {};
     for (var i = 0; i < array.length; i++) {
       var roomTopic = array[i].roomname;
+      // setting default roomname for each message object
       if (!roomTopic) {
-        array[i]['theLounge'] = 'theLounge';
+        array[i]['roomname'] = 'theLounge';
       }
+      // Filling out roomList object
       if (roomList[roomTopic] === undefined) {
         roomList[roomTopic] = [array[i]];
       }
-      if (array[i].roomname !== undefined) {
+      if (roomList[roomTopic] !== undefined) {
         roomList[roomTopic].push(array[i]);
       } 
     }
     return roomList;
   },
+  // beginnings of making a friendslist. supposed to detect if a name is clicked.
   handleUsernameClick: function(username) {
     // document.getElementById("username").addEventListener("click", console.log('hi'));
     $('#main').append('<div>' + username + '</div>');
@@ -97,6 +120,13 @@ var app = {
   // }
 };
 
+// document.ready waits for all html/ javascript to run first,
+  // then it executes any functions inside
+// we used jQuery to select the submitbutton and added an event listener
+  // that would select the userMessage with .val() in the text box
+// and then we send a obj with username/ text to the server.
+  // we also get the new data to display
+
 $(document).ready(function() {
 
   $('#submitButton').on('click', function() {
@@ -110,6 +140,9 @@ $(document).ready(function() {
 
   $('#clearMessages').on('click', function() {
     app.clearMessages();
+  });
+  $('#rooms').change('select', function() {
+    // app.clearMessages($('.chat').not(****));
   });
 
 });
